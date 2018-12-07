568aa426bb773c28bceb0e1a84bd3be8916c3835
zhangfanmark <fzhang@bwh.harvard.edu>
2018-12-07 16:32:20 -0500

    BUG: fix view STFC result

--- SupraThreshold/view_SupraThreshold_results.m
+++ SupraThreshold/view_SupraThreshold_results.m
@@ -12,15 +12,15 @@ if exist(result_folder,'dir')
 end
 
 if strcmp(target_feature, 'LI')
-    write_mrml_bilateral_parcels(atlas_folder, result_folder, r.true_suprathreshold_parcels, ['suprathreshold_parcels_n', num2str(length(r.true_suprathreshold_parcels))]);
+    write_mrml_bilateral_parcels(atlas_folder, result_folder, r.STFC.true_suprathreshold_parcels, ['suprathreshold_parcels_n', num2str(length(r.STFC.true_suprathreshold_parcels))]);
 
 elseif strcmp(target_feature, 'Whole')
-    write_mrml_separated_parcels(atlas_folder, result_folder, r.true_suprathreshold_parcels, 'suprathreshold_parcels');
+    write_mrml_separated_parcels(atlas_folder, result_folder, r.STFC.true_suprathreshold_parcels, 'suprathreshold_parcels');
 
 end
 
-for c_all = 1:length(r.significant_STFCs)
-    parcels_in_significant_STFC = r.significant_STFCs{c_all};
+for c_all = 1:length(r.STFC.significant_STFCs)
+    parcels_in_significant_STFC = r.STFC.significant_STFCs{c_all};
     
     if strcmp(target_feature, 'LI')
         write_mrml_bilateral_parcels(atlas_folder, result_folder, parcels_in_significant_STFC, ['STFC_', num2str(c_all), '_n', num2str(length(parcels_in_significant_STFC))]);
